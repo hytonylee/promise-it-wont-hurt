df746b6805cb9b18b5c8f2763c417722b485b504
Tony Lee <hy.tony.lee@gmail.com>
2019-10-17 20:44:40 -0700

    Complete exercise #3

--- program.js
+++ program.js
@@ -1,14 +1,25 @@
-//// Exercise #2: Fulfilling a Promise
 'use strict'
 require('es6-promise')
+
+//// Exercise #3: To Reject or Not Reject
 const promise = new Promise((fulfill, reject) => {
-    setTimeout(() => {
-        fulfill('FULFILLED!')
-    }, 300)
-}).then(console.log)
+    fulfill('I FIRED')
+    reject(new Error('I DID NOT FIRE'))
+})
+ const onRejected = (error) => {
+      console.log(error.message)
+}
+promise.then(console.log, onRejected)
+
+
+//// Exercise #2: Fulfilling a Promise
+// const promise = new Promise((fulfill, reject) => {
+//     setTimeout(() => {
+//         fulfill('FULFILLED!')
+//     }, 300)
+// }).then(console.log)
 
 //// Exercise #1: Warm Up
-// 'use strict'
 // setTimeout(() => {
 //     console.log('TIMED OUT!')
 // }, 300)
