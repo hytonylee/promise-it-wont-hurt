1467804ee3aaac8cbc2cadc5b391ec7552f603a7
Tony Lee <hy.tony.lee@gmail.com>
2019-10-17 20:22:36 -0700

    Complete exercise #2

--- program.js
+++ program.js
@@ -1,9 +1,14 @@
+//// Exercise #2: Fulfilling a Promise
+'use strict'
+require('es6-promise')
+const promise = new Promise((fulfill, reject) => {
+    setTimeout(() => {
+        fulfill('FULFILLED!')
+    }, 300)
+}).then(console.log)
 
-
-// // Exercise #1: Warm Up
-
+//// Exercise #1: Warm Up
 // 'use strict'
-
 // setTimeout(() => {
 //     console.log('TIMED OUT!')
 // }, 300)
